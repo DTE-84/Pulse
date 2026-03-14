cc3ea84749bc5a88467bf01ec319be020fd28e9d
dte-84 <dte.solutions.llc@gmail.com>
2026-03-14 11:36:37 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/DTE-FinancialSolutions/",
+export default defineConfig({
+  plugins: [expressPlugin()],
+  base: '/PULSE-AI NOVA FINANCE APP/', // MUST match your GitHub repo name exactly
   server: {
     host: "::",
     port: 8080,
@@ -17,14 +17,16 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist/spa",
   },
-  plugins: [react(), expressPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
+ 
   },
-}));
+});
+  
+
 
 function expressPlugin(): Plugin {
   return {
