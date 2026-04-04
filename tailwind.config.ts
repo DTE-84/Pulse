7f06da77efe1abb2de777944d2d7b9486a4ef1ef
dte-84 <dte.solutions.llc@gmail.com>
2026-04-04 18:29:57 -0500

    new commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./index.html", "./src/client/**/*.{ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
