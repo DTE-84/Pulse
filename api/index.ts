95d9dbce4a1ee8ef8fda5c17e204ae5cbc11bd72
dte-84 <dte.solutions.llc@gmail.com>
2026-05-02 10:02:34 -0500

    fix workflow trigger branch to master

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,5 @@
-export default function (req: any, res: any) {
-  res.status(200).json({ 
-    message: "Pulse API Bootloader",
-    timestamp: new Date().toISOString(),
-    env: process.env.NODE_ENV
-  });
-}
+import { createServer } from "../src/server/index";
+
+const app = createServer();
+
+export default app;
