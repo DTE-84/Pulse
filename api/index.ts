c8c448727ed6130a7625a1a5ea1c605f78a97187
dte-84 <dte.solutions.llc@gmail.com>
2026-06-02 01:45:48 -0500

    first commit

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { createServer } from '../src/server/index.js';
+import { createServer } from '../src/server/index';
 
 const app = createServer();
 
