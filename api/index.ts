662bd1c7a7f4b7888d6846feab2e495aba9382fb
dte-84 <dte.solutions.llc@gmail.com>
2026-06-02 15:16:27 -0500

    first commit

--- api/index.ts
+++ api/index.ts
@@ -4,5 +4,6 @@ import { createServer } from '../src/server/index';
 const app = createServer();
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  console.log(`[VERCEL API] Handling ${req.method} ${req.url}`);
   return app(req as any, res as any);
 }
