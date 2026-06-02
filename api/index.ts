e7d43040f5be3296f6c9b5e72850a7405cf538cf
dte-84 <dte.solutions.llc@gmail.com>
2026-06-02 00:37:18 -0500

    first commit

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 // @ts-ignore - Importing from built production bundle
-import app from "../server-dist/production.mjs";
+import app from "../dist/server/production.mjs";
 
 // Vercel expects the function to be the default export
 export default app;
