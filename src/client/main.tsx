76b36fa7a01b56d98f063297c853f8c4daa4407f
dte-84 <dte.solutions.llc@gmail.com>
2026-06-02 01:44:49 -0500

    first commit

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,6 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { registerSW } from 'virtual:pwa-register';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 // Register service worker for PWA
 registerSW({ immediate: true });
 
